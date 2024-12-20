f7563065aab9f0b67b89ca08d27a4767cdc1bcb1
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-12-20 01:48:51 -0500

    fix url

--- voiceforge-ui/src/environments/environment.prod.ts
+++ voiceforge-ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    backendUrl: 'https://voiceforge-backend-e575bd334777.herokuapp.com/', // Replace with your actual backend URL on Heroku or wherever deployed
+    backendUrl: 'https://voiceforge-backend-e575bd334777.herokuapp.com', // Replace with your actual backend URL on Heroku or wherever deployed
   };
   
