ea28ed200fb03c71d3b653a72f153b9b73d9461d
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-12-20 21:40:23 -0500

    try now

--- voiceforge-ui/src/environments/environment.prod.ts
+++ voiceforge-ui/src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: true,
     backendUrl: 'https://voiceforge-backend-e575bd334777.herokuapp.com', // Replace with your actual backend URL on Heroku or wherever deployed
-     auth0Domain: process.env['domain'] || '',
-     auth0ClientId: process.env['clientId'] || '', 
+    // Normally, this is insecure. However, these keys are meant to be public.
+    auth0Domain: 'antfromclick.auth0.com',  // Will be populated dynamically in the app
+    auth0ClientId: 'rJkVz_FNXR8Xe6fv91ddq82v5pHonfYh', // Will be populated dynamically in the app
   };
   
